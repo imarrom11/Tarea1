fa1e13c12168c99c83f8dd2b829aa215256be2a9
Io <imarrom@g.educaand.es>
2024-10-29 22:25:19 +0100

    Programa para sumar numeros

---
+++ demo/src/main/java/com/example/Suma.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class Suma {
+    public static void main(String[] args) {
+        int valor1 = 5;
+        int valor2 = 3;
+
+        System.out.println("La suma de " + valor1 + " más " + valor2 + " es " + (valor1 + valor2));
+    }
+}
